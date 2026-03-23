7dfa1efb324fb2f064e186109907a7e69d370609
NoaxYT <130177719+NoaxYT@users.noreply.github.com>
2026-03-23 13:36:58 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,35 @@
-warn("Loader actif")
+local Players = game:GetService("Players")
+local HttpService = game:GetService("HttpService")
+
+local player = Players.LocalPlayer
+if not player then
+    return
+end
+
+local userId = tostring(player.UserId)
+
+local url = "https://noax-backend-tta2.onrender.com/api/load?userId=" .. userId
+
+local response = game:HttpGet(url)
+
+local ok, data = pcall(function()
+    return HttpService:JSONDecode(response)
+end)
+
+if not ok or not data then
+    warn("Erreur serveur")
+    return
+end
+
+if not data.ok then
+    warn(data.message or "Accès refusé")
+    return
+end
+
+local fn, err = loadstring(data.payload)
+if not fn then
+    warn(err)
+    return
+end
+
+fn()
